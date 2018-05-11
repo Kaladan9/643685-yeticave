49300eb5c629bda35d553ef630eb79b0fd46c49b
Nikolay Ivanov <kaladanhit1@gmail.com>
2018-05-11 22:30:32 +0300

    Получение записей из MySQL

--- data.php
+++ data.php
@@ -13,46 +13,4 @@ $title = 'Главная';
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
 
-$product_categories = [
-    'Доски и лыжи', 'Крепления', 'Ботинки',
-    'Одежда', 'Инструменты', 'Разное'
-];
 
-$product_cards = [
-    0 => [
-        'category' => 'Доски и лыжи',
-        'name' => '2014 Rossignol District Snowboard',
-        'price' => '10999',
-        'product_img_url' => 'img/lot-1.jpg'
-    ],
-    1 => [
-        'category' => 'Доски и лыжи',
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'price' => '159999',
-        'product_img_url' => 'img/lot-2.jpg'
-    ],
-    2 => [
-        'category' => 'Крепления',
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'price' => '8000',
-        'product_img_url' => 'img/lot-3.jpg'
-    ],
-    3 => [
-        'category' => 'Ботинки',
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'price' => '10999',
-        'product_img_url' => 'img/lot-4.jpg'
-    ],
-    4 => [
-        'category' => 'Одежда',
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'price' => '7500',
-        'product_img_url' => 'img/lot-5.jpg'
-    ],
-    5 => [
-        'category' => 'Разное',
-        'name' => 'Маска Oakley Canopy',
-        'price' => '5400',
-        'product_img_url' => 'img/lot-6.jpg'
-    ],
-];
--- index.php
+++ index.php
@@ -2,7 +2,50 @@
 require_once 'functions.php';
 require_once 'data.php';
 
-$page_content = include_templates('templates/index.php', ['product_cards' => $product_cards]);
+
+$con = mysqli_connect('localhost', 'root', '', 'yeticave');
+
+if (!$con) {
+    $sql_error = mysqli_connect_error();
+    print('Ошибка подключения: ' . $sql_error);
+
+} else {
+    $sql_lots = 'SELECT l.name AS name, l.primary_price AS price, l.img_url AS product_img_url, c.name AS category '
+              . 'FROM lots l '
+              . 'JOIN categories c '
+              . 'ON l.category_id = c.id '
+              . 'WHERE NOW() < l.end_date '
+              . 'ORDER BY l.add_date DESC LIMIT 6';
+
+    if ($res = mysqli_query($con, $sql_lots)) {
+        $product_cards = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        $page_content = include_templates('templates/index.php', ['product_cards' => $product_cards]);
+   } else {
+        $sql_error = mysqli_error($con);
+        $page_content = '';
+        print('Ошибка БД: ' . $sql_error);
+   }
+}
+
+$sql = 'SELECT name FROM categories '
+     . 'ORDER BY id';
+
+$result = mysqli_query($con, $sql);
+$product_categories = [];
+
+if ($result) {
+    $cats = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    foreach ($cats as $cat) {
+        $product_categories[] = $cat['name'];
+    }
+
+} else {
+    $sql_error = mysqli_error($con);
+    $page_content = '';
+    print('Ошибка БД: ' . $sql_error);
+}
+
+
 $layout_content = include_templates('templates/layout.php', [
     'page_content' => $page_content,
     'title' => $title,
